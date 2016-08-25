0a7464604d542db4074411744bf6d2dc8e5a5a97
Eunhyung Choi <comdown_1003@naver.com>
2016-08-25 20:21:42 +0900

    modify MainActivity.java 실행가능.

--- proj.android/src/com/melons/btgamecontrollertest/MainActivity.java
+++ proj.android/src/com/melons/btgamecontrollertest/MainActivity.java
@@ -1,24 +1,27 @@
 package com.melons.btgamecontrollertest;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
-import android.view.InputDevice;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.widget.TextView;
+import com.melons.common.Vec2;
+import com.melons.manager.GameControllerManager;
 
-import com.melons.manager.InputManagerCompat;
-import com.melons.manager.InputManagerCompat.InputDeviceListener;
-
-public class MainActivity extends Activity implements InputDeviceListener {
+public class MainActivity extends Activity {
 	private static final String TAG = "MainActivity";
-	
-	private InputManagerCompat _InputManager = null;
 	
 	private TextView _tv1 = null;
 	private TextView _tv2 = null;
 	private TextView _tv3 = null;
+	private TextView _tv4 = null;
+	
+	private DebugHandler _handler = new DebugHandler();
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,46 +32,51 @@ public class MainActivity extends Activity implements InputDeviceListener {
 		_tv1 = (TextView)findViewById(R.id.tv1);
 		_tv2 = (TextView)findViewById(R.id.tv2);
 		_tv3 = (TextView)findViewById(R.id.tv3);
+		_tv4 = (TextView)findViewById(R.id.tv4);
 		
-		_InputManager = InputManagerCompat.Factory.getInputManager(this);
-		_InputManager.registerInputDeviceListener(this, null);
 		
-		findControllers();
+		GameControllerManager.getInstance().init(this, _handler);
 	}
-
-	void findControllers() {
-        int[] deviceIds = _InputManager.getInputDeviceIds();
-        Log.i(TAG, "findControllers deviceCount:["+deviceIds.length+"]");
-        for (int deviceId : deviceIds) {
-        	Log.i(TAG, "findControllers deviceId:"+deviceId);
-            InputDevice dev = _InputManager.getInputDevice(deviceId);
-            int sources = dev.getSources();
-            Log.i(TAG, "findControllers sources:"+sources);
-            // if the device is a gamepad/joystick, create a ship to represent it
-            if (((sources & InputDevice.SOURCE_GAMEPAD) == InputDevice.SOURCE_GAMEPAD) ||
-                    ((sources & InputDevice.SOURCE_JOYSTICK) == InputDevice.SOURCE_JOYSTICK)) {
-                // if the device has a gamepad or joystick
-            	Log.i(TAG, "findControllers JOYSTICK OR GAMEPAD!!!");
-            }
-        }
-    }
 	
-	/*
-	 * implements InputDeviceListener
-	 */
-	@Override
-	public void onInputDeviceAdded(int deviceId) {
-		Log.i(TAG, "onInputDeviceAdded deviceId:"+deviceId);
-	}
-
-	@Override
-	public void onInputDeviceChanged(int deviceId) {
-		Log.i(TAG, "onInputDeviceChanged deviceId:"+deviceId);
-	}
-
-	@Override
-	public void onInputDeviceRemoved(int deviceId) {
-		Log.i(TAG, "onInputDeviceRemoved deviceId:"+deviceId);
+	@SuppressLint("HandlerLeak")
+	class DebugHandler extends Handler {
+		@Override
+		public void handleMessage(Message msg) {
+			super.handleMessage(msg);
+			
+			switch (msg.what) {
+			case GameControllerManager.MSG_WHAT_BUTTON_A:
+				_tv1.setText("KeyDown [BUTTON_A] repeatCount:"+msg.arg1+" time:"+msg.arg2);
+				break;
+			case GameControllerManager.MSG_WHAT_BUTTON_B:
+				_tv2.setText("KeyDown [BUTTON_B] repeatCount:"+msg.arg1+" time:"+msg.arg2);
+				break;
+			case GameControllerManager.MSG_WHAT_JOYSTICK_MOVE:
+			{
+				int eventTime = msg.arg1;
+				
+				float x = -1.0f;
+				float y = -1.0f;
+				Vec2 xy = (Vec2)msg.obj;
+				if (xy != null) {
+					x = xy.x;
+					y = xy.y;
+				}
+				
+				_tv3.setText("x:["+x+"] y:["+y+"]");
+				_tv4.setText("time:"+eventTime);
+				
+				if (x == 0 && y == 0) {
+					_tv3.setBackgroundColor(Color.TRANSPARENT);
+					_tv4.setBackgroundColor(Color.TRANSPARENT);
+				}else{
+					_tv3.setBackgroundColor(Color.RED);
+					_tv4.setBackgroundColor(Color.GREEN);
+				}
+			}
+				break;
+			}
+		}
 	}
 
 	/*
@@ -77,60 +85,34 @@ public class MainActivity extends Activity implements InputDeviceListener {
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 		//Log.i(TAG, "onKeyDown keyCode:"+keyCode+" event:"+event);
-		boolean handled = false;
-		if ((event.getSource() & InputDevice.SOURCE_DPAD) == InputDevice.SOURCE_DPAD) {
-			//if (event.getRepeatCount() == 0) {
-				if (keyCode == KeyEvent.KEYCODE_DPAD_CENTER
-						|| keyCode == KeyEvent.KEYCODE_BUTTON_A)
-				{
-					Log.i(TAG, "onKeyDown CENTER or BTN_A");
-					_tv1.setText("KeyDown BTN_A :"+event.getEventTime()+" repeatCount:"+event.getRepeatCount());
-					handled = true;
-				}else{
-					Log.i(TAG, "onKeyDown ELSE event:"+event);
-				}
-			//}
-			if (handled) {
-				return true;
-			}
-		}
+
+		if (GameControllerManager.getInstance().handleKeyDown(keyCode, event))
+			return true;
 		
 		return super.onKeyDown(keyCode, event);
 	}
 
+	/*
 	@Override
 	public boolean onKeyUp(int keyCode, KeyEvent event) {
-		//Log.i(TAG, "onKeyUp keyCode:"+keyCode+" event:"+event);
-		boolean handled = false;
-		if ((event.getSource() & InputDevice.SOURCE_DPAD) == InputDevice.SOURCE_DPAD) {
-			//if (event.getRepeatCount() == 0) {
-				if (keyCode == KeyEvent.KEYCODE_DPAD_CENTER
-						|| keyCode == KeyEvent.KEYCODE_BUTTON_A)
-				{
-					Log.i(TAG, "onKeyUp CENTER or BTN_A");
-					_tv2.setText("KeyUp BTN_A :"+event.getEventTime()+" repeatCount:"+event.getRepeatCount());
-					handled = true;
-				}else{
-					Log.i(TAG, "onKeyUp ELSE event:"+event);
-				}
-			//}
-			if (handled) {
-				return true;
-			}
-		}
-		
 		return super.onKeyUp(keyCode, event);
 	}
+	*/
 
 	@Override
 	public boolean onGenericMotionEvent(MotionEvent event) {
 		//Log.i(TAG, "onGenericMotionEvent event:"+event);
-		
-		if (_InputManager != null)
-			_InputManager.onGenericMotionEvent(event);
+
+		if (GameControllerManager.getInstance().handleGenericMotionEvent(event))
+			return true;
 		
 		return super.onGenericMotionEvent(event);
 	}
-	
+
+ 	@Override
+	public void onBackPressed() {
+		Log.i(TAG,"onBackPressed");
+		//super.onBackPressed();
+	}
 	
 }
